5798c5ea1b43647f5b5244c0daa3ec5ce762d6ed
vinodsm31 <53043725+vinodsm31@users.noreply.github.com>
2019-09-05 14:43:19 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,83 @@
+package com.training;
+
+public class Vehicle {  
+
+	 
+
+ 	private String make;  
+
+ 	private String model;  
+
+ 	private int year; 
+
+ 	public int getYear() { 
+
+ 		return year; 
+
+ 	} 
+
+ 	public void setYear(int year) { 
+
+ 		this.year = year; 
+
+ 	} 
+
+ 	private int milage; 
+
+ 	private double price;  
+
+  	  
+
+  	public String getMake() {  
+
+  		return make;  
+
+  	}  
+
+  	public void setMake(String make) {  
+
+  		this.make = make;  
+
+ 	}  
+
+  	public String getModel() {  
+
+  		return model;  
+
+  	}  
+
+  	public void setModel(String model) {  
+
+  		this.model = model;  
+
+ 	}  
+
+  	public int getMilage() { 
+
+ 		return milage; 
+
+ 	} 
+
+ 	public void setMilage(int milage) { 
+
+ 		this.milage = milage; 
+
+ 	} 
+
+ 	public double getPrice() {  
+
+  		return price;  
+
+  	}  
+
+  	public void setPrice(double price) {  
+
+  		this.price = price;  
+
+  	}  
+
+   
+
+   
+
+  }  
